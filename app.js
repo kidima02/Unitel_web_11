d1a887cb11a050801a9ee91cd34123c339993e6a
kidima02 <121957902+kidima02@users.noreply.github.com>
2023-01-06 21:49:47 +0100

    commit int
    Projecto-1 frontbox

---
+++ app.js
@@ -0,0 +1,13 @@
+function name(params) {
+    
+} menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains ('open')){
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon') 
+    }else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src="
+    }
+    
+}
